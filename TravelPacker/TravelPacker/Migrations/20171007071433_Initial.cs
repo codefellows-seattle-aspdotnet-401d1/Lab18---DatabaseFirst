00f1ab0072dde75d29a7d16e3ff8af977df110ae
Brandon Little <brlittle86@gmail.com>
2017-10-07 00:15:14 -0700

    added initial migration and created server

---
+++ TravelPacker/TravelPacker/Migrations/20171007071433_Initial.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TravelPacker.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Destination",
+                columns: table => new
+                {
+                    DestinationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Climate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LocationName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destination", x => x.DestinationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18, 2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DestinationProducts",
+                columns: table => new
+                {
+                    DestinationProductsID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DestinationID = table.Column<int>(type: "int", nullable: false),
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DestinationProducts", x => x.DestinationProductsID);
+                    table.ForeignKey(
+                        name: "FK_DestinationProducts_Destination_DestinationID",
+                        column: x => x.DestinationID,
+                        principalTable: "Destination",
+                        principalColumn: "DestinationID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DestinationProducts_Product_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Product",
+                        principalColumn: "ProductID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DestinationProducts_DestinationID",
+                table: "DestinationProducts",
+                column: "DestinationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DestinationProducts_ProductID",
+                table: "DestinationProducts",
+                column: "ProductID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DestinationProducts");
+
+            migrationBuilder.DropTable(
+                name: "Destination");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
