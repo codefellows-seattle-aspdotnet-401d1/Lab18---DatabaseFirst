70d98cc2a0cfe7b9cb28812f79764c1cc35bdf33
George Gaylor <georgegaylor83@outlook.com>
2017-10-06 22:57:51 -0700

    Intialized project

---
+++ Lab18George/Lab18George/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab18George.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
