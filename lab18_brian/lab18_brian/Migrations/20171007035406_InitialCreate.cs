adacfb30da2b38bc24021a834be70ee837262ba4
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-06 20:54:50 -0700

    Added Migration and updated database

---
+++ lab18_brian/lab18_brian/Migrations/20171007035406_InitialCreate.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace lab18_brian.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackingList",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackingList", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackingListItems",
+                columns: table => new
+                {
+                    packinglistID = table.Column<int>(nullable: false),
+                    itemID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackingListItems", x => new { x.packinglistID, x.itemID });
+                    table.UniqueConstraint("AK_PackingListItems_itemID_packinglistID", x => new { x.itemID, x.packinglistID });
+                    table.ForeignKey(
+                        name: "FK_PackingListItems_Item_itemID",
+                        column: x => x.itemID,
+                        principalTable: "Item",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PackingListItems_PackingList_packinglistID",
+                        column: x => x.packinglistID,
+                        principalTable: "PackingList",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PackingListItems");
+
+            migrationBuilder.DropTable(
+                name: "Item");
+
+            migrationBuilder.DropTable(
+                name: "PackingList");
+        }
+    }
+}
