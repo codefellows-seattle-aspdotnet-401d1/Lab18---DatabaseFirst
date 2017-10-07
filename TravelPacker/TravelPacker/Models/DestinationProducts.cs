3e116b610d8af65cc336f087aaf1ade77a555505
Brandon Little <brlittle86@gmail.com>
2017-10-07 00:06:25 -0700

    defined my DestinationProducts Model

--- TravelPacker/TravelPacker/Models/DestinationProducts.cs
+++ TravelPacker/TravelPacker/Models/DestinationProducts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace TravelPacker.Models
 {
     public class DestinationProducts
     {
+        [Key]
+        public int DestinationID { get; set; }
+        [Key]
+        public int ProductID { get; set; }
+
+        public Destination Destination { get; set; }
+        public Product Product { get; set; }
     }
 }
